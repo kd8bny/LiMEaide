92f55157f19f0af5f39ae0f2c6c481f92c32c2a5
kd8bny <kd8bny@gmail.com>
2017-01-31 20:50:07 -0600

    Update to python3 Change password gathering Fix all other major issues

--- limeaide.py
+++ limeaide.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-import sys, datetime, os, argparse
+import sys, datetime, os, argparse, getpass
 from session import Session
 from client import Client
 
@@ -10,41 +10,46 @@ class Limeaide(object):
     def __init__(self):
         super(Limeaide, self).__init__()
         self.lime_dir = './tools/LiME/src/'
+        self.lime_rdir = './lime/'
+        self.output_dir = './output/'
         self.lime = ['disk.c', 'lime.h', 'main.c', 'Makefile', 'tcp.c']
         self.remote_session = None
 
     def send_lime(self):
         print("sending LiME")
-        self.remote_session.exec_cmd('mkdir lime', False)
-        self.remote_session.exec_cmd('cd lime/', False)
+        self.remote_session.exec_cmd('mkdir %s' %self.lime_rdir, False)
         for file in self.lime:
-            self.remote_session.put_sftp(self.lime_dir, file)
+            self.remote_session.put_sftp(self.lime_dir, self.lime_rdir, file)
         print("done.")
 
     def build_lime(self, client):
         print("building kernel module")
-        self.remote_session.exec_cmd('make', False)
-        self.remote_session.exec_cmd('insmod ./lime/lime.ko "path=%s format=raw dio=0"' %client.output, True)
+        self.remote_session.exec_cmd('cd %s; make' %self.lime_rdir, False)
+        self.remote_session.exec_cmd('mv %s/lime*.ko .' %self.lime_rdir, False)
+        self.remote_session.exec_cmd('insmod lime*.ko "path=%s format=raw dio=0"' %client.output, True)
         print("done.")
 
-    def get_lime(self):
+    def get_lime(self, client):
+        print("Changing permissions")
+        self.remote_session.exec_cmd('chmod 755 %s' %client.output, True)
         print("Beam me up Scotty")
-        self.remote_session.pull_sftp(self.lime_dir, file)
+        self.remote_session.pull_sftp(".", self.output_dir, client.output)
 
-    def clean(self):
+    def clean(self, client):
         print("cleaning up...")
-        self.remote_session.exec_cmd('cd .. && rm -r lime/', True)
-        self.remote_session.close()
-        print("done.")
+        self.remote_session.exec_cmd('rm -r lime* %s' %client.output, True)
+        print("Removing LKM...standby")
+        self.remote_session.exec_cmd('rmmod lime.ko', True)
+        print("Memory extraction is complete")
 
     def main(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("remote", help="remote host IP")
-        
+
         parser.add_argument("-s", "--sudoer", help="use a sudo user instead default: root")
         parser.add_argument("-o", "--output", help="name the outputfile")
         args = parser.parse_args()
-        
+
         client = Client()
         client.ip = args.remote
         if args.sudoer != None:
@@ -52,15 +57,16 @@ class Limeaide(object):
             client.is_sudoer = True
         if args.output != None:
             client.output = args.output
-        
+
         print("Attempting secure connection %s@%s" %(client.user, client.ip))
         print("Password for %s:" %client.user)
-        client.pass_ = raw_input()
+        client.pass_ = getpass.getpass()
 
         self.remote_session = Session(client)
         self.send_lime()
         self.build_lime(client)
-        #self.clean()        
+        self.get_lime(client)
+        self.clean(client)
 
 if __name__ == '__main__':
     Limeaide().main()
--- session.py
+++ session.py
@@ -1,14 +1,12 @@
 #/bin/python
 
-import paramiko, logging
+import paramiko, sys
 
 
 class Session(object):
     """Session will take care of all the backend communications"""
     def __init__(self, client):
         super(Session, self).__init__()
-        logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger(__name__)
         self.client = client
         self.session = paramiko.SSHClient()
         self.session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -24,21 +22,27 @@ class Session(object):
         else:
             stdin, stdout, stderr = self.session.exec_command(cmd)
 
-        if stdout != None:
-            for line in stdout:
-                self.logger.info(line.strip('\n'))
-        if stderr != None:
-            for line in stderr:
-                self.logger.error(line.strip('\n'))
+        [print(line.strip('\n')) for line in stdout]
+        if not stderr:
+            print(len(stderr))
+            [print(line.strip('\n')) for line in stderr]
+            sys.exit()
 
-    def pull_sftp(self, dir, file):
-        """Called when data needs to be pulled from remote system"""
-        sftp = self.session.open_sftp()
-        sftp.get(dir + file, file)
-        sftp.close()
 
-    def put_sftp(self, dir, file):
-        """Called when data is to be sent to remote system"""
+        return stderr, stdout
+
+    def pull_sftp(self, rdir, ldir, file):
+        """Called when data needs to be pulled from remote system (remote dir, local dir, file)"""
         sftp = self.session.open_sftp()
-        sftp.put(dir + file, file)
-        sftp.close()
+        if rdir:
+            sftp.chdir(rdir)
+        sftp.get(file, ldir + file)
+        sftp.close
+
+    def put_sftp(self, ldir, rdir, file):
+        """Called when data is to be sent to remote system (local dir, remote dir, file)"""
+        sftp = self.session.open_sftp()
+        if rdir:
+            sftp.chdir(rdir)
+        sftp.put(ldir + file, file)
+        sftp.close
