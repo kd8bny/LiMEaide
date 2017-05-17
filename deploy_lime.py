d7a7b66e238b34d3a2540025f8505b31fadd4611
kd8bny <kd8bny@gmail.com>
2017-05-16 19:49:06 -0600

    pep8

--- deploy_lime.py
+++ deploy_lime.py
@@ -1,6 +1,9 @@
 #!/bin/python
 
-import sys, datetime, os
+import sys
+import os
+import datetime
+
 from client import Client
 
 
@@ -16,7 +19,7 @@ class LimeDeploy(object):
 
     def send_lime(self):
         print("sending LiME")
-        self.remote_session.exec_cmd('mkdir %s' %self.lime_rdir, False)
+        self.remote_session.exec_cmd('mkdir %s' % self.lime_rdir, False)
         for file in self.lime_src:
             self.remote_session.put_sftp(self.lime_dir, self.lime_rdir, file)
         print("done.")
@@ -24,27 +27,34 @@ class LimeDeploy(object):
     def build_lime(self):
         stdout = self.remote_session.exec_cmd('uname -r', False)
         self.client.kver = stdout[0].strip()
-        self.client.module = 'lime-%s.ko' %self.client.kver
-        print("building kernel module %s" %self.client.kver)
-        self.remote_session.exec_cmd('cd %s; make' %self.lime_rdir, False)
+        self.client.module = 'lime-%s.ko' % self.client.kver
+        print("building kernel module %s" % self.client.kver)
+        self.remote_session.exec_cmd('cd %s; make' % self.lime_rdir, False)
         print("Installing LKM and retrieving RAM")
-        self.remote_session.exec_cmd('insmod %s%s "path=%s%s format=lime dio=0"'
-                %(self.lime_rdir, self.client.module, self.lime_rdir, self.client.output), True)
+        self.remote_session.exec_cmd(
+            'insmod %s%s "path=%s%s format=lime dio=0"'
+            % (self.lime_rdir, self.client.module, self.lime_rdir,
+                self.client.output), True)
         print("done.")
 
     def get_lime(self):
         print("Changing permissions")
-        self.remote_session.exec_cmd('chmod 755 %s%s' %(self.lime_rdir, self.client.output), True)
+        self.remote_session.exec_cmd(
+            'chmod 755 %s%s' % (self.lime_rdir, self.client.output), True)
 
         if self.client.compress:
             print("Creating Bzip2...compressing the following")
-            self.remote_session.exec_cmd('tar -jv --remove-files -f %s%s.bz2 -c %s%s'
-                    %(self.lime_rdir, self.client.output, self.lime_rdir, self.client.output), True)
+            self.remote_session.exec_cmd(
+                'tar -jv --remove-files -f %s%s.bz2 -c %s%s'
+                % (self.lime_rdir, self.client.output, self.lime_rdir,
+                    self.client.output), True)
             self.client.output += ".bz2"
 
         print("Beam me up Scotty")
-        self.remote_session.pull_sftp(self.lime_rdir, self.client.output_dir, self.client.output)
-        self.remote_session.pull_sftp(self.lime_rdir, self.client.output_dir, self.client.module)
+        self.remote_session.pull_sftp(
+            self.lime_rdir, self.client.output_dir, self.client.output)
+        self.remote_session.pull_sftp(
+            self.lime_rdir, self.client.output_dir, self.client.module)
 
     def main(self):
         self.send_lime()
