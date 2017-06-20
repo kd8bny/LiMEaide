f53df0b21b78a80db73a8b9c3dd2f0e4aaaa3996
kd8bny <kd8bny@gmail.com>
2017-06-20 15:40:06 -0400

    Copy profile instead of move.

--- deploy_volatility.py
+++ deploy_volatility.py
@@ -55,7 +55,7 @@ class VolDeploy(object):
         self.get_profile()
 
         if vol_dir != 'None':
-            shutil.move(self.output_dir +
+            shutil.copy(self.output_dir +
                         self.client.profile['profile'], vol_dir +
                         self.client.profile['profile'])
         cprint("Profile generation complete run 'vol.py --info | " +
