39d3f8b02c5c459d6713d9499a9e91592300eb4b
kd8bny <kd8bny@gmail.com>
2018-12-10 21:33:50 -0600

    Add headers

--- lib/transfer/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-import functools
-import paramiko
-from termcolor import colored, cprint
-from termcolor import colored
--- lib/transfer/network.py
+++ lib/transfer/network.py
@@ -1,3 +1,23 @@
+# LiMEaide
+# Copyright (c) 2011-2018 Daryl Bennett
+
+# Author:
+# Daryl Bennett - kd8bny@gmail.com
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 import functools
 from threading import Event, Thread
 import queue
